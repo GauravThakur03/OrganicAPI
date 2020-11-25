b09c0856f9ffe4d857e2243685a01dc1829f5ca8
Gaurav Thakur <gaurav.thakur@yash.com>
2020-11-25 15:53:30 +0530

    updated port to 8080 from 9000

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.get('/ekart/areas', function (req, res) {
     });
 })
 
-var server = app.listen(9000, function () {
+var server = app.listen(8080, function () {
    var host = server.address().address;
    var port = server.address().port;
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Mamidikayalu app listening at http://%s:%s", host, port)
 })
